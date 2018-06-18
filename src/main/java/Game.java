2c340596810204ce100b0c5806b9d619cdb08a08
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-18 14:00:02 +0200

    Add graphics to game frame

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-
 public class Game extends JFrame {
 
     public Game() throws HeadlessException {
--- src/main/java/GraphicsComponent.java
+++ src/main/java/GraphicsComponent.java
@@ -1,4 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GraphicsComponent extends JComponent {
+
+    @Override
+    public Dimension getMinimumSize() {
+        return new Dimension(400, 300);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(800, 600);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        Dimension d = getSize();
+        super.paintComponent(g);
+        g.setColor(Color.black);
+        g.fillRect(0, 0, d.width, d.height);
+    }
 }
