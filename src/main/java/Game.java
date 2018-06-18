edc7132fd2c080e861c46ed50d7b387bbf079436
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-18 13:47:25 +0200

    Create game frame

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,6 +5,9 @@ public class Game extends JFrame {
 
     public Game() throws HeadlessException {
         setTitle("Game");
-
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
     }
 }
