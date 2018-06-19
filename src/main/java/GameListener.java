2cf8dad93c57b1b306454884dabdec80eca05e52
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-19 10:18:20 +0200

    Add key listeners

--- src/main/java/GameListener.java
+++ src/main/java/GameListener.java
@@ -6,8 +6,12 @@ import java.awt.event.KeyListener;
 public class GameListener implements KeyListener {
 
     private Scenery scenery;
+    private GraphicsComponent gc;
+    private int ox = 0;
+    private int oy = 0;
 
-    public GameListener(Scenery scenery) {
+    public GameListener(GraphicsComponent gc, Scenery scenery) {
+        this.gc = gc;
         this.scenery = scenery;
     }
 
@@ -16,10 +20,30 @@ public class GameListener implements KeyListener {
     }
 
     public void keyPressed(KeyEvent e) {
-
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            oy = 1;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            oy = -1;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            ox = -1;
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            ox = 1;
+        }
+        updateLocation();
     }
 
     public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_DOWN) {
+            oy = 0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            ox = 0;
+        }
+        updateLocation();
+    }
 
+    private void updateLocation() {
+        scenery.getPlayer().move(ox, oy);
     }
 }
